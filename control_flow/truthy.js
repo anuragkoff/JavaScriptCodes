1e11b34302d028657faa5ef36120e33c4c60d18f
Anurag kumar <112425461+anuragkoff@users.noreply.github.com>
2024-10-04 06:58:40 +0000

    modified:   control_flow/truthy.js 	modified:   control_flow/truthy.js

--- control_flow/truthy.js
+++ control_flow/truthy.js
@@ -5,3 +5,12 @@ if (userEmail) {
 } else {
     console.log("Not have the email.");   
 }
+
+
+
+// NULLISH COALESCING OPERATOR (??): null undefined
+let val = 5 ?? 10
+console.log(val); //5
+
+// sometimes when we call from database like firebase etc. it does not response directly and returns two values,
+// there is chance of getting a null response or undefined
